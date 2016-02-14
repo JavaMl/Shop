9a7e3703c9dc36b28550907f59ecd780685fb7fc
dalongn <974238197@qq.com>
2016-02-14 20:45:21 +0800

    如何发布工程到git

--- src/A.java
+++ src/A.java
@@ -6,7 +6,8 @@ public class A {
 		System.out.println("Hello World");
 
 		// https://github.com/JavaMl/Shop.git
-		// 如何导出 git工程 http://blog.csdn.net/hil2000/article/details/8566456
+		// 如何发布工程到git   http://blog.csdn.net/student_2008/article/details/17118607
+		// 如何从git导出工程          http://blog.csdn.net/hil2000/article/details/8566456
 		// 提交测试 格式化代码
 	}
 
