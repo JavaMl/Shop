4b2920338e8c56c736348818fcbc19c05ffe637e
dalongn <974238197@qq.com>
2016-02-14 17:37:58 +0800

    如何导出git工程

--- src/A.java
+++ src/A.java
@@ -4,6 +4,9 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     System.out.println("Hello World");
+    
+    //https://github.com/JavaMl/Shop.git
+    //如何导出 git工程   http://blog.csdn.net/hil2000/article/details/8566456
 	}
 
 }
